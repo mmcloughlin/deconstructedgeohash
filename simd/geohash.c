81f32136947a18778b8c8317c45134b6016492de
Michael McLoughlin <mmcloughlin@gmail.com>
2018-07-16 22:37:12 -0700

    something that works

--- simd/geohash.c
+++ simd/geohash.c
@@ -33,36 +33,69 @@ void print_uint32(__m256i x)
   printf("\n");
 }
 
+// spread the low 32 bits of each 64-bit lane into the even bit positions of
+// the lane.
+inline __m256i spread(__m256i x)
+{
+  x = _mm256_and_si256(x, _mm256_set1_epi64x(0x00000000ffffffff));
+
+  x = _mm256_or_si256(x, _mm256_slli_epi64(x, 16));
+  x = _mm256_and_si256(x, _mm256_set1_epi64x(0x0000ffff0000ffff));
+
+  x = _mm256_or_si256(x, _mm256_slli_epi64(x, 8));
+  x = _mm256_and_si256(x, _mm256_set1_epi64x(0x00ff00ff00ff00ff));
+
+  x = _mm256_or_si256(x, _mm256_slli_epi64(x, 4));
+  x = _mm256_and_si256(x, _mm256_set1_epi64x(0x0f0f0f0f0f0f0f0f));
+
+  x = _mm256_or_si256(x, _mm256_slli_epi64(x, 2));
+  x = _mm256_and_si256(x, _mm256_set1_epi64x(0x3333333333333333));
+
+  x = _mm256_or_si256(x, _mm256_slli_epi64(x, 1));
+  x = _mm256_and_si256(x, _mm256_set1_epi64x(0x5555555555555555));
+
+  return x;
+}
+
 // encode_int encodes 4 (lat, lng) points.
-void encode_int(double *points, uint64_t *output)
+void encode_int(double *lat, double *lng, uint64_t *output)
 {
   // Quantize.
-  __m256i q[2];
-  for(int i = 0; i < 2; i++) {
-    __m256i p = _mm256_loadu_pd(points + 4*i);
-    p = _mm256_mul_pd(p, _mm256_set_pd(1/360.0, 1/180.0, 1/360.0, 1/180.0));
-    p = _mm256_add_pd(p, _mm256_set1_pd(1.5));
-    q[i] = _mm256_srli_epi64(_mm256_castpd_si256(p), 20);
-  }
+  __m256d latq = _mm256_loadu_pd(lat);
+  latq = _mm256_mul_pd(latq, _mm256_set1_pd(1/180.0));
+  latq = _mm256_add_pd(latq, _mm256_set1_pd(1.5));
+  __m256i lati = _mm256_srli_epi64(_mm256_castpd_si256(latq), 20);
+
+  __m256d lngq = _mm256_loadu_pd(lng);
+  lngq = _mm256_mul_pd(lngq, _mm256_set1_pd(1/360.0));
+  lngq = _mm256_add_pd(lngq, _mm256_set1_pd(1.5));
+  __m256i lngi = _mm256_srli_epi64(_mm256_castpd_si256(lngq), 20);
 
   // Spread
-  
+  __m256i hash = _mm256_or_si256(spread(lati), _mm256_slli_epi64(spread(lngi), 1));
+  _mm256_storeu_si256((__m256i *)output, hash);
 }
 
 int main(int argc, char **argv)
 {
-  double point[8];
-  uint64_t hash[4];
+  double lat[NUM_TEST_VECTORS];
+  double lng[NUM_TEST_VECTORS];
+  uint64_t hash[NUM_TEST_VECTORS];
 
-  assert(NUM_TEST_VECTORS == 4);
-  for(int i = 0; i < 4; i++) {
-    point[2*i] = test_vectors[i].lat;
-    point[2*i+1] = test_vectors[i].lng;
+  for(int i = 0; i < NUM_TEST_VECTORS; i++) {
+    lat[i] = test_vectors[i].lat;
+    lng[i] = test_vectors[i].lng;
   }
 
-  encode_int(point, hash);
+  encode_int(lat, lng, hash);
 
-  for(int i = 0; i < 4; i++) {
-    printf("hash[%d] = %016" PRIx64 "\texpect %016" PRIx64 "\n", i, hash[i], test_vectors[i].hash);
+  for(int i = 0; i < NUM_TEST_VECTORS; i++) {
+    if(hash[i] != test_vectors[i].hash) {
+      printf("FAIL hash[%d] = %016" PRIx64 "\texpect %016" PRIx64 "\n", i, hash[i], test_vectors[i].hash);
+      return 1;
+    }
   }
+
+  printf("pass\n");
+  return 0;
 }
